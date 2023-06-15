369ce70a52c5af7498dd76b612a83dee6d6284ab
fredacee <111201560+fredacee@users.noreply.github.com>
2023-06-15 16:41:28 +0100

    Added bootstap and wrapped tha app in a container

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by{""}
-        <a href="https://www.ichatwithfreda@yahoo.com" target="_blank">
-          Winifred Ayaga
-        </a>
-        {""}
-        and is{""}
-        <a
-          href="https://github.com/fredacee/react-weather-project"
-          target="_blank"
-        >
-          open sourced o GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <footer>
+          This project was coded by{""}
+          <a href="https://www.ichatwithfreda@yahoo.com" target="_blank">
+            Winifred Ayaga
+          </a>
+          {""}
+          and is{""}
+          <a
+            href="https://github.com/fredacee/react-weather-project"
+            target="_blank"
+          >
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
