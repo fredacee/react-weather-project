ab814c21d550f9e3be97285d60ddad27f93d1d4a
fredacee <111201560+fredacee@users.noreply.github.com>
2023-06-15 16:01:05 +0100

    Added my GitHub url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer>
+        This project was coded by{""}
+        <a href="https://www.ichatwithfreda@yahoo.com" target="_blank">
+          Winifred Ayaga
+        </a>
+        {""}
+        and is{""}
+        <a
+          href="https://github.com/fredacee/react-weather-project"
+          target="_blank"
+        >
+          open sourced o GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
