97cbb92af56d5675e46b71a98fad79c34e1c7aa5
fredacee <111201560+fredacee@users.noreply.github.com>
2023-06-28 16:42:12 +0100

    installed the animated weather icon i changed the CSS a little bit i also created a new component that i included i inside the weatherInfo.js component which is rendering the component with right icon code

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       date: new Date(response.data.dt * 1000),
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/cloudy.png",
+      icon: response.data.weather[0].icon,
       wind: response.data.wind.speed,
       city: response.data.name,
     });
@@ -58,7 +58,7 @@ export default function Weather(props) {
             </div>
           </div>
         </form>
-        <WeatherInfo info={weatherData} />
+        <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
